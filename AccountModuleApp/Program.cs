f51c68b5258b5b0e3dcd4276d3cae925cb588c44
Awais JMM <jmm.awaiskiyani@gmail.com>
2025-01-22 12:02:31 +0500

    add Account service

--- AccountModuleApp/Program.cs
+++ AccountModuleApp/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<BranchService>();
+builder.Services.AddScoped<BranchService>();
 // Add DbContext Services
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
---
+++ AccountModuleApp/Services/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using AccountModuleApp.Data;
+
+namespace AccountModuleApp.Services
+{
+    public class AccountService
+    {
+        private readonly AppDbContext appDbContext;
+        public AccountService(AppDbContext context)
+        {
+            appDbContext = context;
+        }
+
+        public async Task<bool> AddAccount()
+        {
+            return true;
+        }
+    }
+}
